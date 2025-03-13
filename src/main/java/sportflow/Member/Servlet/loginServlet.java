4aa0fb0f5c152be7b990ac9fb5210eb62181bd4e
krrsalmane <karrounsalmane@gmail.com>
2025-03-13 12:54:00 +0000

    logout for member

--- src/main/java/sportflow/Member/Servlet/loginServlet.java
+++ src/main/java/sportflow/Member/Servlet/loginServlet.java
@@ -1,5 +1,4 @@
 package sportflow.Member.Servlet;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
---
+++ src/main/java/sportflow/Member/Servlet/logoutServlet.java
@@ -0,0 +1,27 @@
+package sportflow.Member.Servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class logoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); // Get session if exists
+        if (session != null) {
+            session.invalidate(); // Destroy session
+        }
+
+        // Redirect to login page after logout
+        response.sendRedirect("login.jsp");
+    }
+}
+
